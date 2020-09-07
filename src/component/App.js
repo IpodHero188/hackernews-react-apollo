e62e2c18336be17f089d2c7639a6e545c5a541f3
IpodHero188 <DanielMaclin@gmail.com>
2020-09-07 05:28:49 -0500

    initial commit. Finished up to fetching actual data from prisma server

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../style/App.css';
+import LinkList from './LinkList';
+
+function App() {
+  return (
+    <div className="App">
+      <LinkList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/LinkList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from './Link';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+export default function LinkList () {
+
+    const FEED_QUERY = gql 
+    `{
+        feed {
+        links {
+          id,
+          createdAt,
+          description,
+          url
+        }
+      }
+    }`;
+
+    return (
+        <Query query={FEED_QUERY}>
+            {({loading, error, data}) => {
+                if (loading) {return <div>Fetching...</div>}
+                if(error) {return <div>Error</div>}
+
+                const linksToRender = data.feed.links;
+
+                return (
+                    <div>
+                        {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                    </div>
+                );
+            }}
+        </Query>
+    );
+};
